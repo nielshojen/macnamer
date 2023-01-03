2dbb686dee6c0faebaa498647c0220dc1bf94df9
Niels Højen <niels@hojen.net>
2023-01-03 16:18:45 +0100

    disable south

--- macnamer/example_settings.py
+++ macnamer/example_settings.py
@@ -164,7 +164,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'namer',
-    'south',
+    # 'south',
     'bootstrap_toolkit',
 )
 

7d2fe3794159b1cd7fb43ae5af3e9d5ce4b8a4c7
Niels Højen <niels@hojen.net>
2023-01-04 15:51:00 +0100

    fix views

--- namer/urls.py
+++ namer/urls.py
@@ -6,27 +6,27 @@ from . import views
 
 urlpatterns = [
     #front. page
-    url(r'^$', views.index, name='home'),
+    url(r'^$', views.index),
     #new group
-    url(r'^group/new/', views.new_computer_group, name='new_computer_group'),
+    url(r'^group/new/', views.new_computer_group),
     #edit group
-    url(r'^group/edit/(?P<group_id>.+)/', views.edit_computer_group, name='edit_computer_group'),
+    url(r'^group/edit/(?P<group_id>.+)/', views.edit_computer_group),
     #new computer
-    url(r'^computer/new/(?P<group_id>.+)/', views.new_computer, name='new_computer'),
+    url(r'^computer/new/(?P<group_id>.+)/', views.new_computer),
     #edit computer
-    url(r'^computer/edit/(?P<computer_id>.+)/', views.edit_computer, name='edit_computer'),
+    url(r'^computer/edit/(?P<computer_id>.+)/', views.edit_computer),
     #delete computer
-    url(r'^computer/delete/(?P<computer_id>.+)/', views.delete_computer, name='delete_computer'),
+    url(r'^computer/delete/(?P<computer_id>.+)/', views.delete_computer),
     #new network
-    url(r'^network/new/(?P<group_id>.+)/', views.new_network, name='new_network'),
+    url(r'^network/new/(?P<group_id>.+)/', views.new_network),
     #edit network
-    url(r'^network/edit/(?P<network_id>.+)/', views.edit_network, name='edit_network'),
+    url(r'^network/edit/(?P<network_id>.+)/', views.edit_network),
     #delete network
-    url(r'^network/delete/(?P<network_id>.+)/', views.delete_network, name='delete_network'),
+    url(r'^network/delete/(?P<network_id>.+)/', views.delete_network),
     #show network
-    url(r'^network/show/(?P<group_id>.+)/', views.show_network, name='show_network'),
+    url(r'^network/show/(?P<group_id>.+)/', views.show_network),
     #show group
-    url(r'^group/show/(?P<group_id>.+)/', views.show_group, name='show_group'),
+    url(r'^group/show/(?P<group_id>.+)/', views.show_group),
     #get json info
-    url(r'^checkin/', 'checkin', name='checkin'),
+    url(r'^checkin/', views.checkin),
 ]

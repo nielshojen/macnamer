ae865d5ce5cb297242933a7a2bc24d5ab871a631
Niels Højen <niels@hojen.net>
2023-01-05 22:00:13 +0100

    last urls test

--- macnamer/urls.py
+++ macnamer/urls.py
@@ -8,20 +8,13 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    # url(r'^macnamer/', include('macnamer.foo.urls')),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    #path('login/', LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.logout_then_login, name='logout'),
-    #path('logout/', auth_views.logout_then_login, name='logout'),
     url(r'^changepassword/$', auth_views.PasswordChangeView.as_view(), name='changepassword'),
     url(r'^changepassword/done/$', auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
-   	#url(r'^', include('namer.urls')),
-   	path('', include('namer.urls')),
-    # Uncomment the admin/doc line below to enable admin documentation:
+   	url(r'^', include('namer.urls')),
+   	#path('', include('namer.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', admin.site.urls),
-    #url(r'^$', 'namer.views.index', name='home'),
 ]

f14130156908d6446ed054492d13ccf36f64b1fa
Niels Højen <niels@hojen.net>
2023-01-05 11:57:50 +0100

    change templates

--- macnamer/urls.py
+++ macnamer/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^changepassword/$', auth_views.PasswordChangeView.as_view()),
     url(r'^changepassword/done/$', auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
-   	url(r'^', include('namer.urls'), name='home'),
+   	url(r'^', include('namer.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 

7a871a1f956723f4d25e316d5866bfaa0841dad7
Niels Højen <niels@hojen.net>
2023-01-11 11:49:27 +0100

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -4,9 +4,17 @@ cd $APP_DIR
 ADMIN_PASS=${ADMIN_PASS:-}
 mkdir -p db
 chown -R app:app $APP_DIR
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
+
+if [ "$MIGRATE_FROM_SOUTH" = "True" ] ; then
+  find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+  find . -path "*/migrations/*.pyc"  -delete
+  python3 manage.py makemigrations --noinput
+  python manage.py migrate --fake-initial
+else
+  python3 manage.py makemigrations --noinput
+  python3 manage.py migrate --noinput
+  python3 manage.py collectstatic --noinput
+fi
 
 if [ ! -z "$ADMIN_PASS" ] ; then
   python3 manage.py update_admin_user --username=admin --password=$ADMIN_PASS
